5b5293c180f8acb55520fe4e6891f621b340bfef
yashbheke <42708078+yashbheke@users.noreply.github.com>
2020-08-22 07:23:53 -0700

    upto door
    changes upto door

---
+++ Assets/door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public Animation anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        foreach (AnimationState state in anim)
+        {
+            state.speed = 0.5F;
+        }
+    }
+}
