f479b4344090359b4f759b849d3427e55ce18ff9
yashbheke <42708078+yashbheke@users.noreply.github.com>
2020-08-23 07:22:45 -0700

    door script test
    door script needs to be checked

---
+++ Assets/doorcontroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorcontroller : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Door")
+        {
+            Animator anim = other.GetComponentInChildren<Animator>();
+            if (Input.GetKeyDown(KeyCode.E))
+                anim.SetTrigger("opendoor");
+        }
+    }
+}
