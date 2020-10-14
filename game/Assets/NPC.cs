ac9be593fe42c324246cac8f4c2b4a24b8ddef09
geeky8 <65672285+geeky8@users.noreply.github.com>
2020-10-14 20:17:47 +0530

    player_enemy

---
+++ game/Assets/NPC.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.AI;
+public class NPC : MonoBehaviour
+{   
+    GameObject player; 
+    public Transform nps;
+    NavMeshAgent enemy;
+    public Animator anim;
+    float range = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+        nps = GetComponent<Transform>();
+        if(player == null)
+        {
+           Debug.Log("Fuck");  
+		}
+        enemy = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(Vector3.Distance(player.transform.position,nps.position) <= range)
+        { 
+              anim.SetBool("isIdle",false); 
+              if(player != null)
+              {
+                  if(enemy.remainingDistance != Mathf.Infinity && enemy.remainingDistance <= enemy.stoppingDistance)
+                  {
+                      enemy.destination = player.transform.position;
+                      anim.SetBool("isRunning",false);
+                      anim.SetBool("isAttacking",true);
+                  }
+                  else
+                  {
+                      anim.SetBool("isRunning",true);
+                      anim.SetBool("isAttacking",false);
+                      enemy.destination = player.transform.position;  
+		          }
+		      }
+		}
+        else
+        {
+              anim.SetBool("isIdle",true);  
+		}
+     }
+    
+}
---
+++ game/Assets/charcaters/player/move.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{   float speed = 6;
+    float rotspeed = 80;
+    float rot = 0f;
+    float gravity = 8;
+    float lookSpeed= 2.0f;
+    float lookXLimit= 45.0f;
+
+    public Camera playerCamera;
+
+    Vector3 movedir = Vector3.zero;
+    Vector2 rotation = Vector2.zero;
+
+    CharacterController controller;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController> ();
+        anim = GetComponent<Animator> ();
+        rotation.y = transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        GetInput();
+    }
+    void movement()
+    {
+       if(controller.isGrounded)
+       {
+            if(Input.GetKey(KeyCode.W))
+            {   if(anim.GetBool("attack") == true)
+                {
+                   return;        
+				}
+                else if(anim.GetBool("attack") == false)
+                {
+                    anim.SetBool("run",true);
+                    anim.SetInteger("conditon", 1);
+                    movedir = new Vector3(0,0,1);
+                    movedir *= speed;
+                    movedir = transform.TransformDirection (movedir);   
+                    if(anim.GetBool("run")==true)
+                    {           
+                         rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+                         rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+                         rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+                         playerCamera.transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+                         transform.eulerAngles = new Vector2(0, rotation.y);  
+		            }
+				}
+			}
+            if(Input.GetKeyUp(KeyCode.W))
+            {
+                anim.SetBool("run",false);
+                anim.SetInteger("conditon", 0);
+                movedir = new Vector3(0,0,0);
+			}
+	    }
+        rot += Input.GetAxis("Horizontal") * rotspeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0,rot,0);
+        movedir.y -= gravity * Time.deltaTime;
+        controller.Move(movedir * Time.deltaTime);
+       
+	}
+    void GetInput()
+    {
+        if(controller.isGrounded)
+        {
+           if(Input.GetMouseButtonDown(0))
+           {
+              if(anim.GetBool("run") == true)
+              {
+                  anim.SetBool("run",false); 
+                  anim.SetInteger("conditon",0);
+			  }
+              if(anim.GetBool("run") == false)
+              {
+                  attacking();     
+			  }
+		   }
+		}
+	}
+    void attacking()
+    {
+        StartCoroutine(AttackRoutine());
+	}
+    IEnumerator AttackRoutine()
+    {
+        anim.SetBool("attack",true);
+        anim.SetInteger("conditon",2);
+        yield return new WaitForSeconds(1);
+        anim.SetInteger("conditon",0);
+        anim.SetBool("attack",false);
+	}
+}
